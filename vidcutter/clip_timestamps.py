e2706a519de44afbe05497d53fb26a68dd2f5ed3
anton <morzhakovanton@gmail.com>
2024-04-27 15:39:13 +0300

    clip timestamps init commit

---
+++ vidcutter/clip_timestamps.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from PyQt5.QtCore import QTime
+
+
+class ClipTimestamps:
+    class TimestampType(Enum):
+        LEFT = 1
+        RIGHT = 2
+        DOUBLE_SIDED = 3
+
+    def __init__(self, timestamp: QTime, timestamp_type: TimestampType):
+        self.timestamp_ = timestamp
+        self.timestamp_type_ = timestamp_type
+
+    @property
+    def timestamp(self):
+        return self.timestamp_
+
+    @timestamp.setter
+    def timestamp(self, timestamp: QTime):
+        self.timestamp_ = timestamp
+
+    @property
+    def timestamp_type(self):
+        return  self.timestamp_type_
+
+    @timestamp_type.setter
+    def timestamp_type(self, timestamp_type: TimestampType):
+        self.timestamp_type_ = type
